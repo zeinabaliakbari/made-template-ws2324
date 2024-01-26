303fa070256d9fffd4ddeb0868c8ac5450f414ea
Zeinab Aliakbari Mamaghani <148368514+zeinabaliakbari@users.noreply.github.com>
2024-01-26 15:35:43 +0100

    Update exercise5.py

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -8,31 +8,38 @@ def download_and_extract_gtfs_data(url, zip_file_path, extract_folder):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_folder)
 
- 
+        
 def load_and_filter_stops_data(csv_path):
     columns_to_select = ['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id']
     stops_df = pd.read_csv(csv_path, usecols=columns_to_select, dtype={'stop_id': 'str', 'stop_name': 'str', 'stop_lat': 'float', 'stop_lon': 'float', 'zone_id': 'str'}, encoding='utf-8')
-    stops_df = stops_df[stops_df['zone_id'] == '2001']
-    stops_df = stops_df[(stops_df['stop_lat'] >= -90) & (stops_df['stop_lat'] <= 90) & (stops_df['stop_lon'] >= -90) & (stops_df['stop_lon'] <= 90)]
+    stops_df = stops_df[(stops_df['zone_id'] == '2001') & 
+                        (stops_df['stop_lat'] >= -90) & (stops_df['stop_lat'] <= 90) & 
+                        (stops_df['stop_lon'] >= -90) & (stops_df['stop_lon'] <= 90)]
+    print("Number of rows after filtering:", len(stops_df))
+    print(stops_df.head())
+
     return stops_df
 
 
-def create_database_and_table(conn):
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS stops (
-            stop_id TEXT PRIMARY KEY,
-            stop_name TEXT,
-            stop_lat  REAL ,
-            stop_lon  REAL ,
-            zone_id TEXT
-        )
-    ''')
-    conn.commit()
+def connect_to_database(db_name):
+    conn = sqlite3.connect(db_name)
+    return conn, conn.cursor()
 
-def transfer_data_to_database(conn, stops_df):
-    stops_df.to_sql("stops", conn, index=False, if_exists="replace", dtype={'stop_id': 'TEXT', 'stop_name': 'TEXT', 'stop_lat': 'REAL', 'stop_lon': 'REAL', 'zone_id': 'REAL'})
+def create_sqlite_table(cursor, table_name, sqlite_types):
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        {', '.join([f'{col} {col_type}' for col, col_type in sqlite_types.items()])}
+    );
+    """
+    cursor.execute(create_table_query)
+
+def write_to_sqlite(df, conn, table_name):
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
+
+def close_connection(conn):
     conn.commit()
+    conn.close()
+    print("Connection closed.")
 
 def main():
     url = "https://gtfs.rhoenenergie-bus.de/GTFS.zip"
@@ -48,14 +55,21 @@ def main():
     stops_df = load_and_filter_stops_data(csv_path)
 
     # Create SQLite database and table
-    conn = sqlite3.connect(database_name)
-    create_database_and_table(conn)
+    db_name = "gtfs.sqlite"
+    conn, cursor = connect_to_database(db_name)
 
-    # Transfer data to the database
-    transfer_data_to_database(conn, stops_df)
+    # Create SQLite table with appropriate types
+    sqlite_types = {'stop_id': 'TEXT', 'stop_name': 'TEXT', 
+                    'stop_lat': 'REAL', 'stop_lon': 'REAL', 
+                    'zone_id': 'TEXT'}
+    table_name = 'stops'
+    create_sqlite_table(cursor, table_name, sqlite_types)
+
+    # Write DataFrame to SQLite database
+    write_to_sqlite(stops_df, conn, table_name)
 
     # Close the connection
-    conn.close()
+    close_connection(conn)
 
 if __name__ == "__main__":
     main()
